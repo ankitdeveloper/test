e5a656b4a89c0887d8a2ebdbc8f8eb41dba460c1
Ankit Shah <ankit.s.shhah@gmail.com>
2017-02-21 16:32:34 +0530

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -1,19 +1,19 @@
 <?php
-         $to = "kupendra@simprosys.net";
-         $subject = "This is subject";
-         
-         $message = "<b>This is HTML message.</b>";
-         $message .= "<h1>This is headline.</h1>";
-         
-         $header = "From:ankit@simprosys.com \r\n";
-         $header .= "MIME-Version: 1.0\r\n";
-         $header .= "Content-type: text/html\r\n";
-         
-         $retval = mail ($to,$subject,$message,$header);
-         
-         if( $retval == true ) {
-            echo "Message sent successfully...";
-         }else {
-            echo "Message could not be sent...";
-         }
-      ?>
+$to = "kupendra@simprosys.net";
+$subject = "This is subject";
+
+$message = "<b>This is HTML message.</b>";
+$message .= "<h1>This is headline.</h1>";
+
+$header = "From:ankit@simprosys.com \r\n";
+$header .= "MIME-Version: 1.0\r\n";
+$header .= "Content-type: text/html\r\n";
+
+$retval = mail($to, $subject, $message, $header);
+
+if ($retval == true) {
+    echo "Message sent successfully...";
+} else {
+    echo "Message could not be sent...";
+    print_r(error_get_last());
+}
