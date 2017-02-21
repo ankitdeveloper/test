b93c1275f90676d69b6b5278625493c6ad5d92d8
Ankit Shah <ankit.s.shhah@gmail.com>
2017-02-21 16:36:52 +0530

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -5,7 +5,7 @@ $subject = "This is subject";
 $message = "<b>This is HTML message.</b>";
 $message .= "<h1>This is headline.</h1>";
 
-$header = "From:ankit@simprosys.com \r\n";
+$header = "From:ankit@mysterious-gorge-70744.herokuapp.com \r\n";
 $header .= "MIME-Version: 1.0\r\n";
 $header .= "Content-type: text/html\r\n";
 
